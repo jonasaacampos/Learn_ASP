f89df5155f799a8c56c363216c56e6fd340974ec
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-10-04 10:21:01 -0300

    Update iss-config.bat

--- iss-config.bat
+++ iss-config.bat
@@ -1,51 +1,34 @@
-start / w pkgmgr / iu: IIS - WebServerRole;
-    IIS - WebServer;
-    IIS - CommonHttpFeatures;
-    IIS - StaticContent;
-    IIS - DefaultDocument;
-    IIS - DirectoryBrowsing;
-    IIS - HttpErrors;
-    IIS - HttpRedirect;
-    IIS - ApplicationDevelopment;
-    IIS - ASPNET;
-    IIS - NetFxExtensibility;
-    IIS - ASP;
-    IIS - CGI;
-    IIS - ISAPIExtensions;
-    IIS - ISAPIFilter;
-    IIS - ServerSideIncludes;
-    IIS - HealthAndDiagnostics;
-    IIS - HttpLogging;
-    IIS - LoggingLibraries;
-    IIS - RequestMonitor;
-    IIS - HttpTracing;
-    IIS - CustomLogging;
-    IIS - ODBCLogging;
-    IIS - Security;
-    IIS - BasicAuthentication;
-    IIS - WindowsAuthentication;
-    IIS - DigestAuthentication;
-    IIS - ClientCertificateMappingAuthentication;
-    IIS - IISCertificateMappingAuthentication;
-    IIS - URLAuthorization;
-    IIS - RequestFiltering;
-    IIS - IPSecurity;
-    IIS - Performance;
-    IIS - HttpCompressionStatic;
-    IIS - HttpCompressionDynamic;
-    IIS - WebServerManagementTools;
-    IIS - ManagementConsole;
-    IIS - ManagementScriptingTools;
-    IIS - ManagementService;
-    IIS - IIS6ManagementCompatibility;
-    IIS - Metabase;
-    IIS - WMICompatibility;
-    IIS - LegacyScripts;
-    IIS - LegacySnapIn;
-    IIS - FTPPublishingService;
-    IIS - FTPServer;
-    IIS - FTPManagement;
-    WAS - WindowsActivationService;
-    WAS - ProcessModel;
-    WAS - NetFxEnvironment;
-    WAS - ConfigurationAPI
+::■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+::       	   Projeto: Script para ativação do ISS
+::▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+::	      Data de criação: 04/10/2022
+::	          Responsável: Jonas
+::	  Data de atualização: 
+::	Resp. última Atualiz.: 
+::▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+::	 Funcionalidade: Ativa o ISS para funcionamento local de 
+::					sites em ASP e .NET
+::■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+@echo off
+color 1F
+@echo ===============================================================
+@echo 		Ativando a instalacao do IIS
+@echo ---------------------------------------------------------------
+@echo  		!!!executar como AMINISTRADOR!!!
+@echo ===============================================================
+
+::===============================================================
+:: Sricpt para ativacao do ISS
+::===============================================================
+
+start /w pkgmgr /iu:IIS-WebServerRole;IIS-WebServer;IIS-CommonHttpFeatures;IIS-StaticContent;IIS-DefaultDocument;IIS-DirectoryBrowsing;IIS-HttpErrors;IIS-HttpRedirect;IIS-ApplicationDevelopment;IIS-ASPNET;IIS-NetFxExtensibility;IIS-ASP;IIS-CGI;IIS-ISAPIExtensions;IIS-ISAPIFilter;IIS-ServerSideIncludes;IIS-HealthAndDiagnostics;IIS-HttpLogging;IIS-LoggingLibraries;IIS-RequestMonitor;IIS-HttpTracing;IIS-CustomLogging;IIS-ODBCLogging;IIS-Security;IIS-BasicAuthentication;IIS-WindowsAuthentication;IIS-DigestAuthentication;IIS-ClientCertificateMappingAuthentication;IIS-IISCertificateMappingAuthentication;IIS-URLAuthorization;IIS-RequestFiltering;IIS-IPSecurity;IIS-Performance;IIS-HttpCompressionStatic;IIS-HttpCompressionDynamic;IIS-WebServerManagementTools;IIS-ManagementConsole;IIS-ManagementScriptingTools;IIS-ManagementService;IIS-IIS6ManagementCompatibility;IIS-Metabase;IIS-WMICompatibility;IIS-LegacyScripts;IIS-LegacySnapIn;IIS-FTPPublishingService;IIS-FTPServer;IIS-FTPManagement;WAS-WindowsActivationService;WAS-ProcessModel;WAS-NetFxEnvironment;WAS-ConfigurationAPI;
+
+
+::===============================================================
+:: PopUp de finalização do programa
+::===============================================================
+
+mshta javascript:alert("Sucesso!\n\nInternet Information Services (IIS) esta habilitado e em execucao");close();
+
+@exit
